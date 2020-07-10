5925a4e86038042d6c53e80fafbcd359618648ee
BrunoPittaF <caiobrunopittaf@gmail.com>
2020-07-10 01:09:10 -0300

    pequenos teste de coloração

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,32 +5,50 @@ export const Container = styled.div`
   width: 400px;
   margin: 10% auto;
   height: auto;
-  padding: 10px;
+  padding: 14px 10px;
   background-color: white;
   
   .botoes{
     display: flex;
     justify-content: space-between;
-  }
-
-  h1{
-    justify-self: center;
-  }
-
-  a{
-    justify-self: center;
+    a{
     text-decoration: none;
     color: #0404c3;
     font-size: 14px;
     margin-top: 10px;
+    background-color: #545151;
+    padding: 10px;
+    transition: 0.5s filter;
+    border-radius: 6px;
     &:hover{
       color: orange;
+      filter: contrast(50%);
     }
   }
+    
+  }
+
+  h1{
+    justify-self: center;
+    font-size: 42px;
+    color: black;
+    margin-bottom: 20px;
+   
+  }
+
+
 `;
 
 export const Button = styled.button`
   width: 100%;
+  background-color: #a20808;
+  padding: 8px;
+  color: white;
+  border: none;
+  transition: 0.5s filter;
+  &:hover{
+    filter: brightness(75%);
+  }
   
 
 `;
